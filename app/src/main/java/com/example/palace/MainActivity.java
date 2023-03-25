c4ab0fbf6e5081e160dd5e725ae855be4b16df38
dihyeonee <xnaxna1@naver.com>
2023-03-25 17:50:38 +0900

    네비게이션

--- app/src/main/java/com/example/palace/MainActivity.java
+++ app/src/main/java/com/example/palace/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.palace;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -10,8 +13,11 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
 import com.kakao.network.ErrorResult;
@@ -23,9 +29,55 @@ import com.kakao.util.exception.KakaoException;
 import java.security.MessageDigest;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListFragment listFragment;
+    HomeFragment homeFragment;
+    MyFragment myFragment;
+
+    public void replaceFragment(Fragment fragment) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+
+//        fragmentTransaction.replace(R.id.container, fragment).commit();      // Fragment로 사용할 MainActivity내의 layout공간을 선택합니다.
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listFragment = new ListFragment();
+        homeFragment = new HomeFragment();
+        myFragment = new MyFragment();
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+        BottomNavigationView bottom_menu = findViewById(R.id.bottomMenu);
+
+        bottom_menu.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @SuppressLint("NonConstantResourceId")
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.reservationButton:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, listFragment).commit();
+                        Log.d("LOG", "예약리스트화면");
+                        return true;
+                    case R.id.homeButton:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        Log.d("LOG", "홈화면");
+                        return true;
+                    case R.id.setButton:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, myFragment).commit();
+                        Log.d("LOG", "마이페이지화면");
+                        return true;
+                    //Intent intent = new Intent(getApplicationContext(), OrderList.class);
+                    //startActivity(intent);
+                    //return true;
+                }
+                return false;
+            }
+        });
     }
 }
