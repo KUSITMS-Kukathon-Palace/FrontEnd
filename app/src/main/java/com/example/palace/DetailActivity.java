56fcd568f7124d79ae759dc4ba9b83d755f910f3
dihyeonee <xnaxna1@naver.com>
2023-03-26 06:06:59 +0900

    haha

--- app/src/main/java/com/example/palace/DetailActivity.java
+++ app/src/main/java/com/example/palace/DetailActivity.java
@@ -2,7 +2,10 @@ package com.example.palace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        ImageView detail_bg = (ImageView) findViewById(R.id.detailbg);
+
+        detail_bg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(DetailActivity.this, applyActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/palace/HomeFragment.java
+++ app/src/main/java/com/example/palace/HomeFragment.java
@@ -1,23 +1,14 @@
 package com.example.palace;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import lombok.NonNull;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
+import android.widget.ImageView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,9 +16,6 @@ import retrofit2.Response;
  * create an instance of this fragment.
  */
 public class HomeFragment extends Fragment {
-    public HomeAdapter adapter;
-    public ArrayList<HomeItem> hItem;
-
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -37,7 +25,6 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
 
     public HomeFragment() {
         // Required empty public constructor
@@ -70,79 +57,22 @@ public class HomeFragment extends Fragment {
         }
     }
 
-    @NonNull
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView =(ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
-        RecyclerView recyclerView = rootView.findViewById(R.id.rv);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        // Inflate the layout for this fragment
+        View rootView = (ViewGroup) inflater.inflate(R.layout.fragment_home, container, false);
 
-        adapter = new HomeAdapter();
-        recyclerView.setAdapter(adapter);
-        hItem = new ArrayList<>();
-        hItem.add(new HomeItem("경영관 SKT홀", R.drawable.image_01, "서울특별시 성동구 왕십리로 222 한양대학교 경영관", "세미나실", 300));
-        hItem.add(new HomeItem("한양플라자(학생복지관) 연습실", R.drawable.image_02, "서울특별시 성동구 왕십리로 222 한양대학교 학생복지관", "세미나실", 80));
-        hItem.add(new HomeItem("HIT 6층 세미나실",  R.drawable.image_03, "서울특별시 성동구 왕십리로 222 한양대학교 HIT", "세미나실", 50));
-        hItem.add(new HomeItem("학생회관 체육관", R.drawable.image_04,"서울특별시 성동구 왕십리로 222 학생회관", "체육관", 200));
-        hItem.add(new HomeItem("올림픽 체육관B1", R.drawable.image_05,"서울특별시 성동구 왕십리로 222 올림픽체육관", "체육관", 500));
-        hItem.add(new HomeItem("프로젝트룸Make",  R.drawable.image_06,"서울특별시 동작구 상도로 369 벤처관 3층", "세미나실", 72));
-        hItem.add(new HomeItem("프로젝트룸 Passion",  R.drawable.image_07,"서울특별시 동작구 상도로 369 벤처관 1층", "멀티미디어실", 20));
-        hItem.add(new HomeItem("프로젝트룸Make",  R.drawable.image_08,"서울특별시 동작구 상도로 369 벤처관 2층", "세미나실", 40));
-        hItem.add(new HomeItem("첨단강의실(국제회의장)",R.drawable.image_09, "서울특별시 마포구 와우산로 94", "세미나실", 327));
-        hItem.add(new HomeItem("대회의실", R.drawable.image_10,"서울특별시 마포구 와우산로 94", "세미나실", 200));
-        hItem.add(new HomeItem("세미나실",  R.drawable.image_11,"서울특별시 마포구 와우산로 94", "세미나실", 50));
-//        recyclerView = (RecyclerView) rootView.findViewById(R.id.rv);
-//        list = HomeDataView.createContactList();
-//        recyclerView.setHasFixedSize(true);
-//        homeViewAdapter = new HomeViewAdapter(getActivity(), list);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        recyclerView.setAdapter(homeViewAdapter);
+        ImageView homebg = (ImageView) rootView.findViewById(R.id.home_bg);
+
+        homebg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                startActivity(intent);
+            }
+        });
+
         return rootView;
-//        Log.d("Home", "homefrag");
-//        //백엔드 연동
-//        RetrofitAPI retrofitAPI = RetrofitClient.getInstance().create(RetrofitAPI.class);
-//        Call<List<HomeData>> call = retrofitAPI.getHomeData();
-//        call.enqueue(new Callback<List<HomeData>>() {
-//            //성공
-//            @Override
-//            public void onResponse(Call<List<HomeData>> call, Response<List<HomeData>> response) {
-//                getDataList(response.body());
-//                Log.d("HOME", "성공");
-//            }
-//
-//            //실패
-//            @Override
-//            public void onFailure(Call<List<HomeData>> call, Throwable t) {
-//
-//            }
-//        });
     }
-
-//    private void initUI(ViewGroup rootView)
-//    {
-//        recyclerView = rootView.findViewById(R.id.rv);
-//        LinearLayoutManager lm = new LinearLayoutManager(getContext());
-//        recyclerView.setLayoutManager(lm);
-//        homeViewAdapter = new HomeViewAdapter();
-//        homeViewAdapter.addItem(new HomeDataView("경영관 SKT홀", "서울특별시 성동구 왕십리로 222 한양대학교 경영관", "세미나실", 300));
-//        homeViewAdapter.addItem(new HomeDataView("한양플라자(학생복지관) 연습실", "서울특별시 성동구 왕십리로 222 한양대학교 학생복지관", "세미나실", 80));
-//        homeViewAdapter.addItem(new HomeDataView("HIT 6층 세미나실", "서울특별시 성동구 왕십리로 222 한양대학교 HIT", "세미나실", 50));
-//        homeViewAdapter.addItem(new HomeDataView("학생회관 체육관", "서울특별시 성동구 왕십리로 222 학생회관", "체육관", 200));
-//        homeViewAdapter.addItem(new HomeDataView("올림픽 체육관B1", "서울특별시 성동구 왕십리로 222 올림픽체육관", "체육관", 500));
-//        homeViewAdapter.addItem(new HomeDataView("프로젝트룸Make", "서울특별시 동작구 상도로 369 벤처관 3층", "세미나실", 72));
-//        homeViewAdapter.addItem(new HomeDataView("프로젝트룸 Passion", "서울특별시 동작구 상도로 369 벤처관 1층", "멀티미디어실", 20));
-//        homeViewAdapter.addItem(new HomeDataView("프로젝트룸Make", "서울특별시 동작구 상도로 369 벤처관 2층", "세미나실", 40));
-//        homeViewAdapter.addItem(new HomeDataView("첨단강의실(국제회의장)", "서울특별시 마포구 와우산로 94", "세미나실", 327));
-//        homeViewAdapter.addItem(new HomeDataView("대회의실", "서울특별시 마포구 와우산로 94", "세미나실", 200));
-//        homeViewAdapter.addItem(new HomeDataView("세미나실", "서울특별시 마포구 와우산로 94", "세미나실", 50));
-//    }
-//    private void getDataList(List<HomeData> homeData) {
-//        recyclerView = rootView.findViewById(R.id.rv);
-//        adapter = new HomeAdapter(getActivity(), homeData);
-//        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-//        recyclerView.setAdapter(adapter);
-//        adapter.notifyDataSetChanged();
-//        Log.e("Frag: ", "HOMEFRAG");
-//    }
 }
